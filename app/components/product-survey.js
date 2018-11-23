64b21b7407c0e54eb243860cb0b8c50ad20dbb4f
Lalit <chaudharylalit91@gmail.com>
2018-11-23 09:26:09 +0530

    Added styles for multiple choice questions on product page

--- app/components/product-survey.js
+++ app/components/product-survey.js
@@ -60,7 +60,7 @@ export default Component.extend({
  actions:{
    next(){
      if(this.currentSlide<this.masterSlide.length-1){
-       ++this.currentSlide;
+       set(this, 'currentSlide', get(this, 'currentSlide')+1);
        this.forward($('.flip-page'));
      }else{
        //TODO showError();
@@ -69,8 +69,9 @@ export default Component.extend({
 
    previous(){
      if(this.currentSlide>0){
-       --this.currentSlide;
+       set(this, 'currentSlide', get(this, 'currentSlide')-1);
        this.reverse($('.flip-page'));
+     }else{
        //TODO showError();
      }
    },
@@ -80,53 +81,53 @@ export default Component.extend({
  },
 
 
-forward(x){
-  var center = x.length;
-  for(var y = 0; y<x.length;y++){
-    if(x[y].classList.contains('current-page')){
-      center = y;
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":$('.page'+y).css('z-index')-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
-      x[y].classList.remove('current-page');
-    }else if(y<center){
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":$('.page'+y).css('z-index')-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
-    }else{
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":parseInt($('.page'+y).css('z-index'))+1,"transform":this.matrixFnForwrd($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))+0.2)});
+  forward(x){
+    var center = x.length;
+    for(var y = 0; y<x.length;y++){
+      if(x[y].classList.contains('current-page')){
+        center = y;
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":$('.page'+y).css('z-index')-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
+        x[y].classList.remove('current-page');
+      }else if(y<center){
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":$('.page'+y).css('z-index')-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
+      }else{
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+this.computeValue($('.page'+y).css('left'))-parseFloat(this.left)+'px',"z-index":parseInt($('.page'+y).css('z-index'))+1,"transform":this.matrixFnForwrd($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))+0.2)});
+      }
     }
-  }
-  x[++center].classList.add('current-page');
-},
+    x[++center].classList.add('current-page');
+  },
 
-reverse(x){
-  var center = x.length;
-  for(var y = 0; y<x.length;y++){
-    if(x[y].classList.contains('current-page')){
-      center = y;
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
-      x[y].classList.remove('current-page');
-    }else if(y<center){
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))+1,"transform":this.matrixFnForwrd($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))+0.2)});
-    }else{
-      $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
+  reverse(x){
+    var center = x.length;
+    for(var y = 0; y<x.length;y++){
+      if(x[y].classList.contains('current-page')){
+        center = y;
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
+        x[y].classList.remove('current-page');
+      }else if(y<center){
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))+1,"transform":this.matrixFnForwrd($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))+0.2)});
+      }else{
+        $('.page'+y).css({'transition-timing-function' : 'ease-in','transition':'2s','left':''+(this.computeValue($('.page'+y).css('left'))+parseFloat(this.left))+'px',"z-index":parseInt($('.page'+y).css('z-index'))-1,"transform":this.matrixFnBack($('.page'+y).css('transform')),"opacity":(parseFloat($('.page'+y).css('opacity'))-0.2)});
+      }
     }
+    x[--center].classList.add('current-page');
+  },
+
+
+  goToSlide(x){
+  },
+
+  matrixFnForwrd(matrix){
+    var matArray = matrix.replace(/[^0-9\-.,]/g, '').split(',');
+    return "matrix("+(parseFloat(matArray[0])+0.05)+",0,0,"+(parseFloat(matArray[0])+0.05)+",0,0)";
+  },
+
+  matrixFnBack(matrix){
+    var matArray = matrix.replace(/[^0-9\-.,]/g, '').split(',');
+    return "matrix("+(parseFloat(matArray[0]-0.05))+",0,0,"+(parseFloat(matArray[0]-0.05))+",0,0)";
+  },
+  computeValue(value){
+    var x = value.split('p');
+    return parseInt(x[0]);
   }
-  x[--center].classList.add('current-page');
-},
-
-
-goToSlide(x){
-},
-
-matrixFnForwrd(matrix){
-  var matArray = matrix.replace(/[^0-9\-.,]/g, '').split(',');
-  return "matrix("+(parseFloat(matArray[0])+0.05)+",0,0,"+(parseFloat(matArray[0])+0.05)+",0,0)";
-},
-
-matrixFnBack(matrix){
-  var matArray = matrix.replace(/[^0-9\-.,]/g, '').split(',');
-  return "matrix("+(parseFloat(matArray[0]-0.05))+",0,0,"+(parseFloat(matArray[0]-0.05))+",0,0)";
-},
-computeValue(value){
-  var x = value.split('p');
-  return parseInt(x[0]);
-}
 });
